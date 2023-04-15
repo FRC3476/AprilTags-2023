9404c06be4b40da8a1ccbc8107e17ed31830998a
CoolPolyester <Mkasper045@gmail.com>
2023-04-15 15:02:29 -0700

    Add extra comments to draw_lines

--- graphics.py
+++ graphics.py
@@ -23,10 +23,13 @@ def annotate(frame, detection):
     cv2.putText(frame, "ID: " + str(detection.tag_id), (int(corners[1][0] + 10), int(corners[1][1])),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, white)
 
+
 def draw_lines(frame, intrinsics, lines):
+    # Iterates through groups of 6 coordinates representing the two 3D coordinates of the top and the bottom of the pole
     for i in range(0, len(lines), 6):
+        # p1 and p2 represent the two end points of the pole projected into 2d
         p1 = rs.rs2_project_point_to_pixel(intrinsics, [lines[i], lines[i + 1], lines[i + 2]])
         p2 = rs.rs2_project_point_to_pixel(intrinsics, [lines[i + 3], lines[i + 4], lines[i + 5]])
 
+        # Draws line onto the frame from the two 2D endpoints found
         cv2.line(frame, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])), yellow, width_px)
-
